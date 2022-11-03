2d07e6da24d51b2e650440084bcf815d8115c563
lalitraghu1c <raghuwanshilalit5@gmail.com>
2022-11-04 00:02:54 +0530

    [Lalit] Add: Vowels and Consonant

--- BasicCoreProgramming/Program.cs
+++ BasicCoreProgramming/Program.cs
@@ -10,7 +10,7 @@ namespace BasicCoreProgramming
             while (flag)
             {
                 Console.WriteLine("Welcome to Basic Core Programming Problems" + "\n" + "Enter your Choice");
-                Console.WriteLine("1. Flip Coin" + "\n" + "2. Leap Year" + "\n" + "3. Power of Two" + "\n" + "4. Harmonic Number" + "\n" + "5. Prime Factor" + "\n" + "6. Quotient and Remainder" + "\n" + "7. Swap two Number" + "\n" + "8. Even Odd Number" + "\n" + "9. Exit");
+                Console.WriteLine("1. Flip Coin" + "\n" + "2. Leap Year" + "\n" + "3. Power of Two" + "\n" + "4. Harmonic Number" + "\n" + "5. Prime Factor" + "\n" + "6. Quotient and Remainder" + "\n" + "7. Swap two Number" + "\n" + "8. Even Odd Number" + "\n" + "9. Vowels and Consonant" + "\n" + "10. Exit");
                 int choice = Convert.ToInt32(Console.ReadLine());
                 switch (choice)
                 {
@@ -45,6 +45,10 @@ namespace BasicCoreProgramming
                     case 8:
                         EvenOdd even = new EvenOdd();
                         even.Even();
+                        break;
+                    case 9:
+                        VowelsAndConsonant vowel = new VowelsAndConsonant();
+                        vowel.Vowel();
                         break;
                     default:
                         flag = false;
---
+++ BasicCoreProgramming/VowelsAndConsonant.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgramming
+{
+    public class VowelsAndConsonant
+    {
+        public void Vowel()
+        {
+            Console.WriteLine("Enter Alphabet -");
+            char check = Convert.ToChar(Console.ReadLine().ToLower());
+            switch (check)
+            {
+                case 'a':
+                    Console.WriteLine(check + " is Vowel");
+                    break;
+                case 'e':
+                    Console.WriteLine(check + " is Vowel");
+                    break;
+                case 'i':
+                    Console.WriteLine(check + " is Vowel");
+                    break;
+                case 'o':
+                    Console.WriteLine(check + " is Vowel");
+                    break;
+                case 'u':
+                    Console.WriteLine(check + " is Vowel");
+                    break;
+                default:
+                    Console.WriteLine(check + " is Consonant");
+                    break;
+            }
+        }
+    }
+}
