3454d3219fbd41da8b9e4e473f4158cab7f44cdb
lalitraghu1c <raghuwanshilalit5@gmail.com>
2022-11-03 22:59:18 +0530

    [Lalit] Add: Flip Coin

---
+++ BasicCoreProgramming/FlipCoin.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgramming
+{
+    public class FlipCoin
+    {
+        Random random = new Random();
+        public void Coin()
+        {
+            double head = 0, tail = 0;
+            Console.WriteLine("\n" + "No. of Times you want to Flip the Coin");
+            int times = Convert.ToInt32(Console.ReadLine());
+            double actual_count = times;
+            while (times > 0)
+            {
+                if (random.NextDouble() > 0.5)
+                    head++;
+                else
+                    tail++;
+                times--;
+            }
+            Console.WriteLine("Head will come - " + head);
+            Console.WriteLine("Tail will come - " + tail);
+            double Head_Percentage = (head / actual_count) * 100;
+            double Tail_Percentage = (tail / actual_count) * 100;
+            Console.WriteLine("\n" + "Head Percenage will be - " + Head_Percentage + "\n" + "Tail Percentage will be - " + Tail_Percentage);
+        }
+    }
+}
--- BasicCoreProgramming/Program.cs
+++ BasicCoreProgramming/Program.cs
@@ -5,7 +5,24 @@ namespace BasicCoreProgramming
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Basic Core Programming Problems");
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Welcome to Basic Core Programming Problems" + "\n" + "Enter your Choice");
+                Console.WriteLine("1. Flip Coin" + "\n" + "2. Exit");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        FlipCoin flip = new FlipCoin();
+                        flip.Coin();
+                        break;
+                    case 2:
+                        flag = false;
+                        Console.WriteLine("Try Again");
+                        break;
+                }
+            }
         }
     }
 }
