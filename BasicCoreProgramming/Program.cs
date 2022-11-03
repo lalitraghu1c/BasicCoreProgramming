1d72362ac98d80150e89ff76c3a1dc24dad59ac6
lalitraghu1c <raghuwanshilalit5@gmail.com>
2022-11-03 23:49:10 +0530

    [Lalit] Add: Quotient and Remainder

--- BasicCoreProgramming/Program.cs
+++ BasicCoreProgramming/Program.cs
@@ -10,7 +10,7 @@ namespace BasicCoreProgramming
             while (flag)
             {
                 Console.WriteLine("Welcome to Basic Core Programming Problems" + "\n" + "Enter your Choice");
-                Console.WriteLine("1. Flip Coin" + "\n" + "2. Leap Year" + "\n" + "3. Power of Two" + "\n" + "4. Harmonic Number" + "\n" + "5. Prime Factor" + "\n" + "6. Exit");
+                Console.WriteLine("1. Flip Coin" + "\n" + "2. Leap Year" + "\n" + "3. Power of Two" + "\n" + "4. Harmonic Number" + "\n" + "5. Prime Factor" + "\n" + "6. Quotient and Remainder" + "\n" + "7. Exit");
                 int choice = Convert.ToInt32(Console.ReadLine());
                 switch (choice)
                 {
@@ -33,6 +33,10 @@ namespace BasicCoreProgramming
                     case 5:
                         PrimeFactor prime = new PrimeFactor();
                         prime.Prime();
+                        break;
+                    case 6:
+                        QuotientAndRemainder quotient = new QuotientAndRemainder();
+                        quotient.Quotient();
                         break;
                     default:
                         flag = false;
---
+++ BasicCoreProgramming/QuotientAndRemainder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgramming
+{
+    public class QuotientAndRemainder
+    {
+        public void Quotient()
+        {
+            int quotient = 0, remainder = 0;
+            Console.WriteLine("Enter the values of Dividend");
+            int dividend = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the values of Divisor");
+            int divisor = Convert.ToInt32(Console.ReadLine());
+            quotient = dividend / divisor;
+            remainder = dividend % divisor;
+            Console.WriteLine("Quotient =" + quotient + "\n" + "Ramainder =" + remainder);
+        }
+    }
+}
